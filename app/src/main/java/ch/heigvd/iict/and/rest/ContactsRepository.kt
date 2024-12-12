44f10c0b6b998c6b8ac159c72ed21540d0059180
Tasticoco <114155288+Tasticoco@users.noreply.github.com>
2024-12-12 16:43:49 +0100

    Ajout de commentaire pour expliquer l'implémentation de l'UUID, trop cool!

--- app/src/main/java/ch/heigvd/iict/and/rest/ContactsRepository.kt
+++ app/src/main/java/ch/heigvd/iict/and/rest/ContactsRepository.kt
@@ -4,6 +4,9 @@ import ch.heigvd.iict.and.rest.database.ContactsDao
 
 class ContactsRepository(private val contactsDao: ContactsDao) {
 
+    //TODO: avoir l'UUID afin de pouvoir l'utiliser pour communiquer avec le serveur.
+    // Il faut également mettre les fontions enroll et refresh ici qui seront appelées par le ViewModel.
+
     val allContacts = contactsDao.getAllContactsLiveData()
 
 }
--- app/src/main/java/ch/heigvd/iict/and/rest/MainActivity.kt
+++ app/src/main/java/ch/heigvd/iict/and/rest/MainActivity.kt
@@ -8,6 +8,10 @@ import ch.heigvd.iict.and.rest.ui.theme.MyComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
 
+    //TODO: Récupérer l'UUID dans les préférences depuis ici et s'il existe le passer au ViewModel
+    // qui le donnera au repository, observer la valeur de l'UUID dans le ViewModel pour savoir
+    // s'il change à cause de enroll et finalement le sauvegarder dans les préférences à la fin.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
