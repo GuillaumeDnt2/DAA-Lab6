b6763f561c43ddcfe8ea9e2960469bfed97f52f1
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2025-01-09 16:22:44 +0100

    ajout toContactDTO

--- app/src/main/java/ch/heigvd/iict/and/rest/models/Contact.kt
+++ app/src/main/java/ch/heigvd/iict/and/rest/models/Contact.kt
@@ -5,15 +5,47 @@ import androidx.room.PrimaryKey
 import java.util.*
 
 @Entity
-data class Contact(@PrimaryKey(autoGenerate = true) var id: Long? = null,
-              var name: String,
-              var firstname: String?,
-              var birthday : Calendar?,
-              var email: String?,
-              var address: String?,
-              var zip: String?,
-              var city: String?,
-              var type: PhoneType?,
-              var phoneNumber: String?,
-              var remoteId: Long?,
-              var status: Status?)
+data class Contact(
+    @PrimaryKey(autoGenerate = true) var id: Long? = null,
+    var name: String,
+    var firstname: String?,
+    var birthday: Calendar?,
+    var email: String?,
+    var address: String?,
+    var zip: String?,
+    var city: String?,
+    var type: PhoneType?,
+    var phoneNumber: String?,
+    var remoteId: Long?,
+    var status: Status?
+)
+
+fun Contact.toContactDTO(): ContactDTO? {
+    if (this.remoteId == null
+        || this.name == null
+        || this.firstname == null
+        || this.birthday == null
+        || this.email == null
+        || this.address == null
+        || this.zip == null
+        || this.city == null
+        || this.type == null
+        || this.phoneNumber == null
+    ) {
+        return null
+    } else {
+        return ContactDTO(
+            this.remoteId!!,
+            this.name,
+            this.firstname,
+            this.birthday.toString(),
+            this.email,
+            this.address,
+            this.zip,
+            this.city,
+            this.type.toString(),
+            this.phoneNumber
+        )
+
+    }
+}
